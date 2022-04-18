d6e8b8c18a914d59d1191ecf8d4ab5806ed8b700
Mahima SIngh <mahima.ms0007@gmail.com>
2022-04-18 15:16:17 +0200

    First Commit
    Asset Import
    Scene setup
    Player Control basics
    Prefab
    Forward Movement projectile
    Instantiate projectile
    Forward movement animals
    Destroying out of bounds gameobj

---
+++ AnimalFarm/Assets/Scripts/DestroyOutofBounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutofBounds : MonoBehaviour
+{
+    private float lowerBound = -5f;
+    private float topBound = 35f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ AnimalFarm/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    public float xRange = 15;
+    public GameObject food;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch a projectile
+            Instantiate(food, transform.position, food.transform.rotation);
+        }
+        
+    }
+}
